6f1ae21cbf623b249f6c857c79e8b9657094d2fb
Wander Anjos <wander.anjosb@gmail.com>
2023-05-01 20:37:06 -0300

    Finalização do projeto ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,67 @@
 import logoDark from './assets/logotipo-dark.svg';
-import logoLight from './assets/logotipo-dark.svg';
 import styles from './App.module.css';
 import './global.css';
-import { CheckItem } from './components/CheckItem';
+import { CheckItem, CheckItemType } from './components/CheckItem';
 import { Plus } from '@phosphor-icons/react';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 export function App(){
 
-  const mockupCheckItem = [
-    {
-      id: "A",
-      text: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+  const [items, setItems] = useState<CheckItemType[]>([]);
+
+  const dateNow = new Date();
+
+  const dateFormated = {
+    "dayToFormat": format(dateNow, "dd", {locale: ptBR}),
+    "monthToFormat": format(dateNow, "MMM", {locale: ptBR}),
+    "yearToFormat": format(dateNow, "yyyy", {locale: ptBR})
+  };
+
+  function checkItem(checked:boolean, id: string){
+    const listAfterUpdatedCheck = items.map(e=>{
+      if(e.id == id) e.isCompleted = checked;
+      return e;
+    });
+    setItems(listAfterUpdatedCheck);
+  }
+
+  const totalCheckedItems = items.filter((e) => {
+    if(e.isCompleted) return true;
+    return false;
+  }).length;
+
+  const totalItems = items.length;
+
+  const [newTask, setNewTask] = useState("");
+
+  function handleNewTaskChange(event:ChangeEvent<HTMLInputElement>){
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateNewTask(event:FormEvent){
+
+    event.preventDefault();
+
+    const newItem = {
+      id: uuidv4(),
+      content: newTask,
       isCompleted: false
-    },
-    {
-      id: "B",
-      text: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. Integer urna interdum massa libero auctor neque",
-      isCompleted: false
-    },
-    {
-      id: "C",
-      text: "Integer urna interdum massa libero  Duis vel sed fames integer.",
-      isCompleted: true
     }
-  ];
+
+    setItems([...items, newItem]);
+    setNewTask('');
+  }
+
+  function deleteTask(id: string){
+    const itemsWithoutDeletedOne = items.filter(e =>{
+      return e.id !== id
+    });
+    setItems(itemsWithoutDeletedOne);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -33,30 +71,30 @@ export function App(){
       <section>
         <div className={styles.actions}>
           <div className={styles.dateInfo}>
-            <div className={styles.dateInfoDay}>16</div>
+            <div className={styles.dateInfoDay}>{dateFormated.dayToFormat}</div>
             <div className={styles.dateInfoMonthAndYear}>
-              <p className={styles.dateInfoMonth}>Fev</p>
-              <p className={styles.dateInfoYear}>2023</p>
+              <p className={styles.dateInfoMonth}>{dateFormated.monthToFormat}</p>
+              <p className={styles.dateInfoYear}>{dateFormated.yearToFormat}</p>
             </div>
           </div>
           <div className={styles.formSend}>
-            <form>
+            <form onSubmit={handleCreateNewTask}>
               <div className={styles.formGroup}>
-                <input type="text" placeholder='Adicione uma nova tarefa'/>
+                <input type="text" value={newTask} placeholder='Adicione uma nova tarefa' onChange={handleNewTaskChange}/>
               </div>
               <button className={styles.buttonSend} type='submit'>Adicionar novo item <Plus size={16}/></button>
             </form>
           </div>
           <div className={styles.tasksInfo}>
             <p>Minhas Tarefas</p>
-            <span>4 de 5 Concluídas</span>
+            <span>{totalCheckedItems} de {totalItems} Concluídas!</span>
           </div>
         </div>
       </section>
       <main>
 
         {
-          (mockupCheckItem.length <= 0 ? 
+          (items.length <= 0 ? 
             <div className={styles.nothingContent}>
               <p>
                 <span>Você ainda não tem tarefas cadastradas</span> <br /> Crie Tarefas e organize seus itens a fazer!</p>
@@ -64,8 +102,8 @@ export function App(){
          :  
           <div className={styles.contentList}>
             {
-              mockupCheckItem.map(e=>{
-                return <CheckItem key={e.id} content={e.text} checked={e.isCompleted}></CheckItem>
+              items.map(e=>{
+                return <CheckItem key={e.id} id={e.id} content={e.content} isCompleted={e.isCompleted} onCheckItem={checkItem} onDeleteItem={deleteTask}></CheckItem>
               })
             }
           </div>
--- src/components/CheckItem.tsx
+++ src/components/CheckItem.tsx
@@ -1,17 +1,40 @@
+import React, { ChangeEvent, ChangeEventHandler, FormEvent, useState } from 'react';
 import styles from './CheckItem.module.css';
 import { Trash } from '@phosphor-icons/react';
 
-interface CheckItemProps{
+
+export interface CheckItemType{
+  id: string,
   content: string,
-  checked: boolean
+  isCompleted: boolean
 }
 
-export function CheckItem({content, checked}:CheckItemProps){
+interface CheckItemProps{
+  id: string,
+  content: string,
+  isCompleted: boolean,
+  onCheckItem: (checked:boolean, id: string) => void,
+  onDeleteItem: (id: string) => void
+}
+
+export function CheckItem({id, content, isCompleted, onCheckItem, onDeleteItem}:CheckItemProps){
+
+  const [completedItem, setCompletedItem] = useState(isCompleted);
+
+  function handleCheckItem(event: ChangeEvent<HTMLInputElement>){
+    setCompletedItem(event.target.checked);
+    onCheckItem(!completedItem, id);
+  }
+
+  function handleDeleteItem(){
+    onDeleteItem(id);
+  }
+
   return (
-    <div className={ (checked === false ? styles.checkWrapper : `${styles.checkWrapper} ${styles.checkedItem}`) }>
-      <input className={styles.buttonCheckItem} type="checkbox" checked={checked}/>
+    <div className={ (completedItem === false ? styles.checkWrapper : `${styles.checkWrapper} ${styles.checkedItem}`) }>
+      <input className={styles.buttonCheckItem} type="checkbox" checked={completedItem} onChange={handleCheckItem} />
       <p className={styles.contentCheckItem}>{content}</p>
-      <button className={styles.buttonDelete}><Trash size={16}/></button>
+      <button className={styles.buttonDelete} onClick={handleDeleteItem}><Trash size={16}/></button>
     </div>
   )
 }
